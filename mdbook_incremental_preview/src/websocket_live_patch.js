134085c65edef468e6a47343015c81629d9d89d9
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-07-28 17:47:41 +0800

    fix(highlight): force highlight on patch

--- mdbook_incremental_preview/src/websocket_live_patch.js
+++ mdbook_incremental_preview/src/websocket_live_patch.js
@@ -12,6 +12,8 @@
         contentElement.innerHTML = event.data;
         document.dispatchEvent(new Event("load"));
         if (window.hljs && window.hljs.initHighlighting) {
+            // Re-highlight with highlight.js.
+            window.hljs.initHighlighting.called = false;
             window.hljs.initHighlighting();
         }
     };
